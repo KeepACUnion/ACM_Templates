6c94ecf997d9ca96a036b9db5d282e0650273bfe
Akatsuki <dreandawn@gmail.com>
2018-05-12 01:58:39 +0800

    update dijkstra_heap

---
+++ Graph/dijkstra_heap.cpp
@@ -0,0 +1,28 @@
+#include <queue>
+#include <cstring>
+using namespace std;
+typedef pair<int, int> pii;
+#define mk make_pair
+#define fi first
+#define se second
+const int maxn = 1e5+10;
+vector<pii> G[maxn];
+int cost[maxn];
+void dijkstra(int s)
+{
+    memset(cost, 0x3f, sizeof(cost));
+    cost[s] = 0;
+    priority_queue<pii, vector<pii>, greater<pii> >pq;
+    pq.push(mk(0, s));
+    while(!pq.empty()){
+        pii p = pq.top(); pq.pop();
+        if(cost[p.se] < p.fi)continue;
+        for(int i = 0; i < (int)G[p.se].size(); i++){
+            pii v = G[p.se][i];
+            if(cost[v.fi] > cost[p.se]+v.se){
+                cost[v.fi] = cost[p.se]+v.se;
+                pq.push(mk(cost[v.fi], v.fi));
+            }
+        }
+    }
+}
