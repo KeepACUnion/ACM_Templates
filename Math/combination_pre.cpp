12aed56fc16fcbbcf52bc87263deee2ddff197fb
lych4o <lych@hust.edu.cn>
2018-05-12 20:05:31 +0800

    Update combination_pre.cpp

--- Math/combination_pre.cpp
+++ Math/combination_pre.cpp
@@ -1,7 +1,6 @@
 ///简单递推
-int c[maxn][maxn]
-void init()
-{
+int c[maxn][maxn];
+void init(){
     memset(c,0,sizeof(c));
     c[0][0] = c[1][0] = c[1][1] = 1;
     for(int i = 2; i < maxn; i++){
@@ -24,15 +23,12 @@ void extgcd(ll aa,ll bb,ll& dd,ll& xx,ll& yy){
         yy -= xx*(aa/bb);
     }
 }
-
 ll inv(ll aa,ll mm){
     ll dd, xx, yy;
     extgcd(aa, mm, dd, xx, yy);
     return dd==1?(xx+mm)%mm:-1;
 }
-
-void init()
-{
+void init(){
     fac[0] = 1;
     iv[0] = inv(fac[0], mod);
     for(int i = 1; i < maxn; i++){
@@ -40,9 +36,7 @@ void init()
         iv[i] = inv(fac[i], mod);
     }
 }
-
-ll comb(int x, int y)
-{
+ll comb(int x, int y){
     if(x < y) swap(x,y);
     ll res = (fac[x]*iv[x-y])%mod;
     res = (res*iv[y])%mod;
