4c8d99446dd60ce478f6cc5361a7bd9b99c1ae22
lych4o <lych@hust.edu.cn>
2018-05-12 20:01:59 +0800

    Update pnt_d&c.cpp

--- Graph/pnt_d&c.cpp
+++ Graph/pnt_d&c.cpp
@@ -1,14 +1,11 @@
 const int maxn = 1e4+10;
 int n, head[maxn], val[maxn*2], nxt[maxn*2],
     to[maxn*2], tot;
-
 void add_edg(int u, int v, int w){
     tot++; nxt[tot] = head[u]; to[tot] = v;
     val[tot] = w; head[u] = tot;
 }
-
 int root, mx[maxn], sz[maxn], done[maxn];
-
 void get_root(int u, int f, int tree_size){
     mx[u] = 0; sz[u] = 1;
     for(int i = head[u]; i; i = nxt[i]){
@@ -21,7 +18,6 @@ void get_root(int u, int f, int tree_size){
     mx[u] = max(mx[u], tree_size-sz[u]);
     if(mx[u] < mx[root]) root = u;
 }
-
 void solve(int u, int tree_size){
     root = 0;
     mx[root] = n;
@@ -40,7 +36,6 @@ void solve(int u, int tree_size){
         solve(v, sz[v]);
     }
 }
-
 void init(){
     memset(done, 0, sizeof(done));
     memset(head, 0, sizeof(head));
