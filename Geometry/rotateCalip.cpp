e9241b0633b0be4428ba6c1e36e55075a41e3669
lych4o <lych@hust.edu.cn>
2018-09-08 10:44:48 +0800

    Update rotateCalip.cpp

--- Geometry/rotateCalip.cpp
+++ Geometry/rotateCalip.cpp
@@ -3,6 +3,7 @@ double Dis(Point a, Point b){return Length(a-b);}
 double Area(Point a, Point b, Point c){return Cross(b-a,c-a);}
 double rot_calip(Point* p, int n){
     double res = 0;
+    p[n] = p[0];
     for(int i = 0, j = 1; i < n; i++){
         while(Area(p[i],p[i+1],p[j+1]) > Area(p[i],p[i+1],p[j])) j=(j+1)%n;
         res = max(res, Dis(p[i], p[j]));
