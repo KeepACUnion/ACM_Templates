0e874392ce3d835f9fdb4e05e288af5c2f376bfa
Akatsuki <dreandawn@gmail.com>
2018-09-11 10:29:45 +0800

    update costFlow_dijkstra

--- Graph/costFlow_dijkstra.cpp
+++ Graph/costFlow_dijkstra.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cstring>
 #include <queue>
 using namespace std;
 typedef pair<int, int> pii;
@@ -7,33 +8,33 @@ typedef pair<int, int> pii;
 #define pb push_back
 #define mk make_pair
 const int INF = 0x3f3f3f3f;
-const int maxn = 110;
-const int maxv = 10100;
+const int maxv = 1e4+10;
 int n, m, ans;
-struct es
+struct edge
 {
     int to, cap, rev, cost;
-    es(int to, int cap, int rev, int cost):to(to),cap(cap),rev(rev),cost(cost){}
+    edge(int to, int cap, int rev, int cost):to(to),cap(cap),rev(rev),cost(cost){}
 };
-vector<es> G[maxv];
+vector<edge> G[maxv];
 bool vis[maxv];
 int dist[maxv];
+
 void add_edge(int from, int to, int cap, int cost)
 {
-    G[from].pb(es(to, cap, (int)G[to].size(), cost));
-    G[to].pb(es(from, 0, (int)G[from].size()-1, -cost));
+    G[from].pb(edge(to, cap, (int)G[to].size(), cost));
+    G[to].pb(edge(from, 0, (int)G[from].size()-1, -cost));
 }
+
 bool dijkstra(int s, int t)
 {
-    memset(dist, INF, sizeof(dist));
+    memset(dist, 0x3f, sizeof(dist));
     dist[t] = 0;
     priority_queue<pii, vector<pii>, greater<pii> > pq;
     pq.push(mk(0, t));
     while(!pq.empty()){
         pii u = pq.top(); pq.pop();
         if(dist[u.se] < u.fi)continue;
-        for(int i = 0; i < (int)G[u.se].size(); i++){
-            es &e = G[u.se][i];
+        for(auto &e : G[u.se]){
             if(G[e.to][e.rev].cap && dist[e.to] > dist[u.se]-e.cost){
                 dist[e.to] = dist[u.se]-e.cost;
                 pq.push(mk(dist[e.to], e.to));
@@ -42,14 +43,14 @@ bool dijkstra(int s, int t)
     }
     return dist[s] < INF;
 }
+
 int dfs(int v, int t, int f)
 {
     vis[v] = 1;
     if(v == t)return f;
     int ret = 0;
-    for(int i = 0; i < (int)G[v].size(); i++){
-        es &e = G[v][i];
-        if(!vis[e.to] && e.cap && dist[v] - e.cost == dist[e.to]){
+    for(auto &e : G[v]){
+        if(!vis[e.to] && e.cap > 0 && dist[v]-e.cost == dist[e.to]){
             int d = dfs(e.to, t, min(e.cap, f-ret));
             if(d){
                 ans += e.cost*d;
@@ -62,6 +63,7 @@ int dfs(int v, int t, int f)
     }
     return ret;
 }
+
 int costflow(int s, int t)
 {
     int flow = 0;
