2cb4fc94b77f8ac5b8206bc4cabaef99b62d4e23
Akatsuki <dreandawn@gmail.com>
2018-09-24 20:37:34 +0800

    update partitionTree

--- Data_Structure/partitionTree.cpp
+++ Data_Structure/partitionTree.cpp
@@ -1,8 +1,8 @@
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-const int maxn = 1e5 + 10;
-const int maxd = 18;
+const int maxn = 1e5+10;
+const int maxd = 20;
 typedef struct
 {
     int num[maxn];
@@ -13,22 +13,22 @@ int sorted[maxn];
 void build(int d, int l, int r)
 {
     if(l == r)return;
-    int m = (l + r) >> 1;
-    int need = m - l + 1;
+    int m = (l+r)>>1;
+    int need = m-l+1;
     for(int i = l; i <= r; i++)if(tree[d].num[i] < sorted[m])need--;
-    int p = l, q = m + 1;
+    int p = l, q = m+1;
     for(int i = l, cnt = 0; i <= r; i++){
         int num = tree[d].num[i];
         if(num < sorted[m] || (num == sorted[m] && need)){
             if(num == sorted[m])need--;
             cnt++;
-            tree[d + 1].num[p++] = num;
+            tree[d+1].num[p++] = num;
         }
-        else tree[d + 1].num[q++] = num;
+        else tree[d+1].num[q++] = num;
         tree[d].cnt[i] = cnt;
     }
-    build(d + 1, l, m);
-    build(d + 1, m + 1, r);
+    build(d+1, l, m);
+    build(d+1, m+1, r);
 }
 int query(int d, int l, int r, int ql, int qr, int k)
 {
@@ -36,17 +36,17 @@ int query(int d, int l, int r, int ql, int qr, int k)
     int ly;
     if(l == ql)ly = 0;
     else ly = tree[d].cnt[ql-1];
-    int tot = tree[d].cnt[qr] - ly;
-    int newl, newr, mid = (l + r) / 2;
+    int tot = tree[d].cnt[qr]-ly;
+    int newl, newr, mid = (l+r) / 2;
     if(tot >= k){
-        newl = l + ly;
-        newr = newl + tot - 1;
-        return query(d + 1, l, mid, newl, newr, k);
+        newl = l+ly;
+        newr = newl+tot-1;
+        return query(d+1, l, mid, newl, newr, k);
     }
     else{
-        newl = mid + 1 + (ql - l - ly);
-        newr = newl + ((qr-ql+1)-tot) - 1;
-        return query(d + 1, mid + 1, r,  newl, newr, k - tot);
+        newl = mid+1+(ql-l-ly);
+        newr = newl+((qr-ql+1)-tot)-1;
+        return query(d+1, mid+1, r,  newl, newr, k-tot);
     }
 }
 void solve()
